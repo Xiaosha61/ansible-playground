0729bdf51f828736ea23c53afa9b66ed63c60798
Xiaosha61 <xing.xiaosha61@gmail.com>
2021-01-23 19:52:05 +0100

    chore: add P4_L4_E28_workflow
    * part4/Lesson4/Exercise28, promote to production

--- helper-scripts/create-stack.sh
+++ helper-scripts/create-stack.sh
@@ -1,3 +1,12 @@
+# deploy EC2
+# aws cloudformation deploy \
+#     --template-file cloud-formation/template.yml \
+#     --stack-name my-stack
+
+# deploy S3 bucket and override the manually created one
+S3_BUCKET_NAME="xiaosha-website"
 aws cloudformation deploy \
-    --template-file cloud-formation/template.yml \
-    --stack-name my-stack
+    --template-file cloud-formation/cloudfront.yml \
+    --stack-name production-distro \
+    --parameter-overrides PipelineID="${S3_BUCKET_NAME}" \ # Name of the S3 bucket you created manually.
+    --tags project=udapeople &
